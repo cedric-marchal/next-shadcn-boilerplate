9db7516911a786d02f67438186e5c899d2d05a08
novicode <cedricmarchaldev@gmail.com>
2025-05-05 21:24:01 +0200

    Update environment variables for support email and database connection
    - Replaced NEXT_PUBLIC_EMAIL_CONTACT with NEXT_PUBLIC_SUPPORT_EMAIL in .env-template for clarity.
    - Updated DATABASE_URL in .env-template to include connection details for PostgreSQL.
    - Adjusted env.ts to reflect the changes in environment variable names.
    
    This commit improves environment configuration for better clarity and functionality.

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -9,14 +9,14 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_APP_NAME: z.string(),
     NEXT_PUBLIC_BASE_URL: z.string().url(),
-    NEXT_PUBLIC_EMAIL_CONTACT: z.string().email(),
+    NEXT_PUBLIC_SUPPORT_EMAIL: z.string().email(),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    NEXT_PUBLIC_EMAIL_CONTACT: process.env.NEXT_PUBLIC_EMAIL_CONTACT,
+    NEXT_PUBLIC_SUPPORT_EMAIL: process.env.NEXT_PUBLIC_SUPPORT_EMAIL,
   },
   emptyStringAsUndefined: true,
   skipValidation: false,
